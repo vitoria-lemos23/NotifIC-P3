5275bc482e7464d780e85bbe4dffa571f77c477e
Rian Antônio da Silva Gaião <38548532+ryanthecomic@users.noreply.github.com>
2025-11-08 21:36:06 -0300

    limpar

--- src/backend/services/email_service.py
+++
@@ -1,8 +0,0 @@
-# services/email_service.py
-
-from flask_mail import Message
-from app import mail
-
-def send_email(to, subject, body):
-    msg = Message(subject, recipients=[to], body=body)
-    mail.send(msg)
